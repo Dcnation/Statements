3fb199e6c4e8fa9c1ad3e57352cf3328d4115914
Devin Carlisle <devincarlisle@Devins-Air.attlocal.net>
2022-06-06 09:35:52 -0500

    adding to git

---
+++ Statements/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Number Guessing Game
+            var r = new Random();
+            //var favNumber = r.Next(1, 10);
+            var favNumber = 7;
+
+            Console.WriteLine("Pick a number from 1 - 1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is to low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is to high");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed it");
+
+            }
+
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine(); //evualate a string
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is awesome");
+                    break;
+                default:
+                    Console.WriteLine("I havvent taken that subject yet");
+                    break;
+
+            }
+
+        }
+    }
+
+}
+  
+
